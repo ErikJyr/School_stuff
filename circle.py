2335a86f66e84acbc1112ca4b5f1e36b960fbc21
Erik Jürgenstein <erik.jurgenstein@ametikool.ee>
2025-11-10 13:54:08 +0200

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,8 +1,11 @@
 
 a = 3.14
-b =  input ('RADIUS: ')
+b =  input ('RAADIUS: ')
 
 c = a * (int(b) ** 2)
 d= round(c, 2)
 
-print(d)
+print("PINDALA:", d)
+e= 2*a* int(b)
+f= round(e, 2)
+print("ÜMBERMÕÕT:", f)
