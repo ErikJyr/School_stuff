22c5a2b0f0c0021aaabb9e0913cbda2fa6450bbc
erikjyr <erik.jurgenstein@ametikool.ee>
2025-11-10 13:40:43 +0200

    On branch School_stuff Initial commit

---
+++ EEK-EUR.py
@@ -0,0 +1,8 @@
+
+a = 15.6466
+b =  input ('EEK: ')
+
+c = int(b) / (a)
+d= round(c, 2)
+
+print(d)
---
+++ circle.py
@@ -0,0 +1,8 @@
+
+a = 3.14
+b =  input ('RADIUS: ')
+
+c = a * (int(b) ** 2)
+d= round(c, 2)
+
+print(d)
