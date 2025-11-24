aab5c21216c0c6875aad8861949124889a218fa3
Erik Jürgenstein <erik.jurgenstein@ametikool.ee>
2025-11-24 09:31:36 +0200

    Implement logic to find the biggest of three numbers

---
+++ yl6.py
@@ -0,0 +1,12 @@
+a = float(input("enter 1st num: "))
+b = float(input("enter 2nd num: "))
+c = float(input("enter 3rd num: "))
+
+if a >= b and a >= c:
+    biggest
+elif b >= a and b >= c:
+    biggest = b
+else:
+    biggest = c
+
+print("The biggest number is", biggest)
