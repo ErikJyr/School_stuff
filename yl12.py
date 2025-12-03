3f7464726c6e5796d2514af750f7a422a86ca9bd
ErikJyr <erik.jurgenstein@ametikool.ee>
2025-12-03 13:47:26 +0200

    I honestly dont even know anymore.

---
+++ yl12.py
@@ -0,0 +1,16 @@
+
+fruits = ["banana", "strawberry", "pear"]
+print(fruits)
+print(fruits[0])
+fruits.append("apple")
+print(fruits[-1])
+fruits[1] = "orange"
+print(fruits)
+print("apple" in fruits)
+print(len(fruits))
+fruits.remove("pear")
+print(fruits)
+fruits.reverse()
+print(fruits)
+fruits.sort()
+print(fruits)
