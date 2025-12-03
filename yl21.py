e8a01cff95335fe7270eca34809d25ca462d64f3
ErikJyr <erik.jurgenstein@ametikool.ee>
2025-12-03 15:35:16 +0200

    fixed a bug

--- yl21.py
+++ yl21.py
@@ -6,11 +6,11 @@ print("Arvu arvamise mäng!")
 print("Arvuti mõtles välja arvu 0 kuni 100. Proovi ära arvata!")
 
 while True:
-    pakkumine = int(input("Sinu pakkumine: "))
+    offer = int(input("Sinu pakkumine: "))
 
-    if pakkumine < salajane_arv:
+    if offer < salajane_arv:
         print("Liiga väike! Proovi suuremat arvu.")
-    elif pakkumine > salajane_arv:
+    elif offer > salajane_arv:
         print("Liiga suur! Proovi väiksemat arvu.")
     else:
         print("Õige! Arv oli", salajane_arv)
