06341fb8e5b123b28c680c662f6f66a49bf41a6e
ErikJyr <erik.jurgenstein@ametikool.ee>
2025-12-12 10:38:03 +0200

    yl25.py

--- yl25.py
+++ yl25.py
@@ -1,8 +1,34 @@
-thisdict = {
-  "first_name": "Erik",
-  "last_name": "Jyr",
-  "date_of_birth": 2000,
-  "lives_at":"estonia",
-  "favorite_food": "black forest cake"
+me = {
+    "first_name": "Erik",
+    "last_name": "Jyr",
+    "birth_year": 2009,
+    "location": "Estonia",
+    "favorite_dessert": "Black forest cake"
 }
-print(thisdict)
+
+print(me["location"])
+print(me.get("location"))
+
+me["favorite_dessert"] = "chocolate cake"
+
+for key in me.keys():
+    print(key)
+
+for value in me.values():
+    print(value)
+
+for key in me:
+    print(me[key])
+
+print("id_code" in me)
+print(len(me))
+
+me["height_cm"] = 170
+me.pop("birth_year")
+
+data = {"a": 1, "b": 2}
+copy_data = data
+data.clear()
+
+data = {"a": 1, "b": 2}
+del data
